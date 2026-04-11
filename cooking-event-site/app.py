8d8958f6970bf0df307bfe88b0cd9f960b57aeeb
benjaminogwal <benjaminogwal645@gmail.com>
2026-04-11 22:50:04 +0200

    Update app.py

--- cooking-event-site/app.py
+++ cooking-event-site/app.py
@@ -87,8 +87,11 @@ class Registration(Base):
 
 def normalize_database_url(database_url):
     if database_url.startswith("postgres://"):
-        return database_url.replace("postgres://", "postgresql://", 1)
+        return database_url.replace("postgres://", "postgresql+psycopg://", 1)
+    if database_url.startswith("postgresql://"):
+        return database_url.replace("postgresql://", "postgresql+psycopg://", 1)
     return database_url
+
 
 
 def create_db_engine(database_url):
